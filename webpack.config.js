dbd31126e8ccab919c871664d50ef63f2408e047
Kartik N.V <kartiknanneboina@gmail.com>
2021-09-03 22:26:37 +0530

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,50 @@
 const path = require('path')
+const ExtractTextPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_mdoules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-    mode: 'none'
+
+module.exports = (env) => {
+    const isProduction = env.production
+    const CSSExtract = new ExtractTextPlugin({
+        filename: 'styles.css'
+    })
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_mdoules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    ExtractTextPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        mode: isProduction ? 'production' : 'development'
+    }
 }
