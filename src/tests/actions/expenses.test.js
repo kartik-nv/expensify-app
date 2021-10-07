2bff9dd24fc4730f4859320ec713e17253df324e
Kartik N.V <kartiknanneboina@gmail.com>
2021-10-07 16:28:40 +0530

    Handled Editing of expenses.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
     removeExpense,
     startRemoveExpense,
     setExpenses,
-    startSetExpenses
+    startSetExpenses,
+    startEditExpense
 } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
@@ -56,6 +57,26 @@ test('should setup edit expense object', () => {
         updates: {
             note: 'Hello there!!'
         }
+    })
+})
+
+test('should edit expenses from firebase', (done) => {
+    const store = createMockStore({})
+    const id = expenses[1].id
+    const updates = {
+        description: 'Current'
+    }
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        })
+        return database.ref(`expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val().description).toBe('Current')
+        done()
     })
 })
 
