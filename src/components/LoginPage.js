ef6bb45b63360e2ebcad92643cff1923325b510b
Kartik N.V <kartiknanneboina@gmail.com>
2021-10-20 13:33:14 +0530

    Added Facebook Authentication

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { startLoginWithGoogle, startLoginWithFacebook } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startLoginWithGoogle, startLoginWithFacebook }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expensify</h1>
       <p>It's time to track your expenses without a hitch.</p>
-      <button
-        className="button"
-        onClick={startLogin}>Login with Google</button>
+      <div className="box-layout__login-buttons">
+        <button
+          className="button"
+          onClick={startLoginWithGoogle}
+        >
+          Login with Google
+        </button>
+        <p>or</p>
+        <button
+          className="button"
+          onClick={startLoginWithFacebook}
+        >
+          Login with Facebook
+        </button>
+      </div>
     </div>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogin: () => dispatch(startLogin())
+  startLoginWithGoogle: () => dispatch(startLoginWithGoogle()),
+  startLoginWithFacebook: () => dispatch(startLoginWithFacebook())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
