822cf70a5ec57e2a2177e3b5dd1a9469267c3913
Kartik N.V <kartiknanneboina@gmail.com>
2021-11-01 13:55:45 +0530

    Bug Fixures for homescreen

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage.js'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+}
+
 const store = configureStore();
 const jsx = (
   <Provider store={store}>
